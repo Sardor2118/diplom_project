dcfebc13101e33468b30c8ce9a8606da9c7d8219
Sardor2118 <mo3rus@gmail.com>
2024-04-28 17:48:05 +0500

    28.04.2024

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
 from database import Base, engine
-from pydantic import BaseModel
-import asyncio
-import uvicorn
+from api.users_api.users import user_router
+from api.posts_api.post import posts_router
+from api.comments_api.comments import comment_router
 from fastapi.staticfiles import StaticFiles
 
 # команда для создания всех таблиц в дб
 Base.metadata.create_all(bind=engine)
 app = FastAPI(docs_url="/")
 app.mount("/static", StaticFiles(directory="static"))
-app.include_router()
-app.include_router()
-app.include_router()
-app.include_router()
+app.include_router(user_router)
+app.include_router(comment_router)
+app.include_router(posts_router)
+
